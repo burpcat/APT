220d1a7350e9a337b583f238097df80ed93f363e
Avinash <codekkurama@gmail.com>
2021-10-29 08:23:15 +0530

    aa

---
+++ HackerRank/CPP/Intoduction/inputOutput.cpp
@@ -0,0 +1,16 @@
+// C++ > Introduction > Input and Outpur
+// https://www.hackerrank.com/challenges/cpp-input-and-output/problem
+
+#include <iostream>
+using namespace std;
+
+int a, b, c;
+
+int main()
+{
+
+    cin >> a >> b >> c;
+    cout << a + b + c;
+
+    return 0;
+}
