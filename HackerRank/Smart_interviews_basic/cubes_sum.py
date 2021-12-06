471a5118e52f71c6dd2345041b05b233c81119fa
Avinash <codekurama@gmail.com>
2021-12-06 22:12:35 +0530

    added few more codes

---
+++ HackerRank/Smart_interviews_basic/cubes_sum.py
@@ -0,0 +1,10 @@
+# https://www.hackerrank.com/contests/smart-interviews-basic/challenges/si-basic-squares-sum
+# Smart Interviews Basic > Squares sum
+
+basic_input = input()
+sum =0
+
+for i in range(int(basic_input)+1):
+    sum = sum + (i**3)
+
+print(sum)
---
+++ HackerRank/Smart_interviews_basic/natural_sum.py
@@ -0,0 +1,10 @@
+# https://www.hackerrank.com/contests/smart-interviews-basic/challenges/si-basic-natural-numbers-sum
+# Smart Interviews Basic > Natural numbers sum
+
+basic_input = input()
+sum =0
+
+for i in range(int(basic_input)+1):
+    sum = sum +i
+
+print(sum)
