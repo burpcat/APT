ca8df3a52fac0f485853b2207ceb49a77c99b4ad
Avinash <codekurama@gmail.com>
2021-12-07 21:10:37 +0530

    added num_rev.py

--- HackerRank/Smart_interviews_basic/num_rev.py
+++ HackerRank/Smart_interviews_basic/num_rev.py
@@ -1,3 +1,12 @@
 # https://www.hackerrank.com/contests/smart-interviews-basic/challenges/si-basic-number-reverse
 # Smart Interviews Basic > Number Reverse
 
+basic_input = input()
+main_str = ''
+
+for i in range(len(basic_input)):
+    last_digit = int(basic_input) %10
+    main_str = main_str + str(last_digit)
+    basic_input = int(basic_input) //10
+
+print(main_str)
