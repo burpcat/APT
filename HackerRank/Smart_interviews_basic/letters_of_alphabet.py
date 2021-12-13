2f0064f5f2a2435c27e7d8408334cc21df0987cc
Avinash <codekurama@gmail.com>
2021-12-13 21:27:17 +0530

    changes

--- HackerRank/Smart_interviews_basic/letters_of_alphabet.py
+++ HackerRank/Smart_interviews_basic/letters_of_alphabet.py
@@ -1,3 +1,7 @@
+# https://www.hackerrank.com/contests/smart-interviews-basic/challenges/si-basic-letters-of-the-alphabet/copy-from/1339707694
+# Smart Interviews Basic > Letters of the alphabet
+
+
 basic_input = input().lower()
 some_list = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 for ele in basic_input:
