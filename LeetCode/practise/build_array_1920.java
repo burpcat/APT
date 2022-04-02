c665b3d369f77b35122235ced80f4fd41dad7e0e
Avinash Reddy Arutla <codekurama@gmail.com>
2022-04-02 10:54:20 +0530

    why java sucks?

---
+++ LeetCode/practise/build_array_1920.java
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/build-array-from-permutation/
+// 1920. Build Array from Permutation
+
+class Solution {
+    public int[] buildArray(int[] nums) {
+    
+            
+        int ans[] = new int[nums.length];
+
+        for (int i=0; i<nums.length ; i++){
+            ans[i] = nums[nums[i]];
+        }
+
+        return ans;
+
+        
+    }
+}
